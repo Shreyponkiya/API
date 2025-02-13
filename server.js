cc03cbdb15d01fef874489dc07fb746e26157d9e
Shrey Ponkiya <shreyponkiya@gmail.com>
2025-02-13 11:34:38 +0530

    commit code

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-const jsonServer = require('json-server');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
-// Create the server
-const server = jsonServer.create();
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-// Set the JSON file as the database
-const router = jsonServer.router(path.join(__dirname, 'user.json'));
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
 
-// Middleware for default settings
-const middlewares = jsonServer.defaults();
+    // Load `user.json` from the root folder
+    const filePath = path.join(process.cwd(), 'user.json');
+    const jsonData = fs.readFileSync(filePath, 'utf8');
 
-// Use the middlewares
-server.use(middlewares);
+    res.status(200).json(JSON.parse(jsonData));
+}
 
-// Use the router
-server.use(router);
-console.log("router : ",router) 
-// Start the server
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running at http://localhost:${PORT}`);
-});
+// const jsonServer = require('json-server');
+// const path = require('path');
+
+// // Create the server
+// const server = jsonServer.create();
+
+// // Set the JSON file as the database
+// const router = jsonServer.router(path.join(__dirname, 'user.json'));
+
+// // Middleware for default settings
+// const middlewares = jsonServer.defaults();
+
+// // Use the middlewares
+// server.use(middlewares);
+
+// // Use the router
+// server.use(router);
+
+// // Start the server
+// const PORT = 3000;
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running at http://localhost:${PORT}`);
+// });
